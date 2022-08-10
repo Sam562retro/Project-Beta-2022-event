cdd2a4bfdf7d29cd1a8e85319b081ee3abe9e796
Soham <gsoham562@gmail.com>
2022-08-10 23:03:45 +0530

    bugs

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 app.set('view engine', 'ejs');
 app.use('/css', express.static(path.resolve(__dirname, "assets/css")));
 app.use('/js', express.static(path.resolve(__dirname, "assets/js")));
-app.use('/pics', express.static(path.resolve(__dirname, "assets/pic")));
+app.use('/pics', express.static(path.resolve(__dirname, "assets/pics")));
 
 app.use(bodyParser.urlencoded({extended: true}));
 
